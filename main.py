a2b8f181c6884e45b1c2e90e11653b12bdb871a7
Valentina Leon Beltran <vleonb@unal.edu.co>
2025-06-21 19:27:28 -0500

    correcion 2.0
    Menu de inicio

--- main.py
+++ main.py
@@ -1,4 +1,74 @@
 from restaurante import Restaurante
+from empleado import Empleado
+from staff import Staff
+from JefeMesero import JefeMesero
+from mesa import Mesa
+
+def Menu(restaurante,empleado,rol):
+    if rol == "Staff":
+        print("1. Consultar disponibilidad de mesas")
+        print("2. Salir")
+        opcion = int(input("Ingrese el número de la opción: "))
+
+        if opcion == 1:
+            print("\nConsultando disponibilidad de mesas...")
+            cantidad_mesa = empleado.consultar_disponibilidad(restaurante.mesas)
+            if cantidad_mesa == 0:
+                print("No hay mesas disponibles en el restaurante.")
+            else:
+                print(f"Hay {cantidad_mesa} mesas disponibles en el restaurante.")
+                
+            print("¿Deseas regresar al menú principal? (1: Sí, 2: No)")
+
+            num= int(input("Ingrese su opción: "))
+            if num == 1:
+                Menu(restaurante,empleado, rol)
+            elif num == 2:
+                print("Saliendo del sistema...")
+                main()
+
+            else:
+                print("\nOpción no válida, ingrese una opción válida 1 o 2")
+                Menu(restaurante,empleado, rol)
+
+        elif opcion == 2:
+            print("Saliendo del sistema...")
+            main()
+
+        else:
+            print("\nOpción no válida, ingrese una opción válida 1 o 2")
+            Menu(restaurante,empleado, rol)
+                
+    elif rol == "JefeMesero":
+        print("1. Consultar mesas disponibles")
+        print("2. Salir")
+        opcion = int(input("\nIngrese el número de la opción: "))
+        if opcion == 1:
+            print("\nConsultando mesas disponibles...")
+            lista_mesas_dispo = empleado.consultar_disponibilidad_especifica(restaurante.mesas)
+            print(f"Hay {lista_mesas_dispo} mesas disponibles en el restaurante.")
+            print("\n¿Deseas regresar al menú principal? (1: Sí, 2: No)")
+                
+            num= int(input("\nIngrese su opción: "))
+            if num == 1:
+                Menu(restaurante,empleado, rol)
+            elif num == 2:
+                print("Saliendo del sistema...")
+                main()
+        elif opcion == 2:
+            print("Saliendo del sistema...")
+            main()
+                
+                
+        else:
+            print("\nOpción no válida, ingrese una opción válida 1 o 2")
+            Menu(restaurante,empleado, rol)
+                    
+    else:
+        print("Rol no reconocido, no se puede mostrar el menú.")
+
+
+
 
 def main():
     #Crear instancia del restaurante
@@ -20,10 +90,10 @@ def main():
         if rol == "Staff":
             print(f"\nHas iniciado sesión como {rol}.")
             print(f'¿Que deseas hacer?')
-            restaurante.Menu(empleado, rol)
+            Menu(restaurante,empleado, rol)
         elif rol == "JefeMesero":
             print(f"\nHas iniciado sesión como {rol}.")
             print(f'¿Que deseas hacer?')
-            restaurante.Menu(empleado, rol)
+            Menu(restaurante,empleado, rol)
 
 main()
